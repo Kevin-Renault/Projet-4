6066e324582387150123b50faa8fc5f570e4484c
Kevin Renault <kevin.renault.92@gmail.com>
2026-01-01 18:09:10 +0100

    refactor: add explicit return types and remove all any

--- front/src/app/pages/sessions/components/detail/detail.component.ts
+++ front/src/app/pages/sessions/components/detail/detail.component.ts
@@ -42,17 +42,17 @@ export class DetailComponent implements OnInit {
     this.fetchSession();
   }
 
-  public back() {
+  public back(): void {
     window.history.back();
   }
 
   public delete(): void {
     this.sessionApiService
       .delete(this.sessionId)
-      .subscribe((_: any) => {
-          this.matSnackBar.open('Session deleted !', 'Close', { duration: 3000 });
-          this.router.navigate(['sessions']);
-        }
+      .subscribe(() => {
+        this.matSnackBar.open('Session deleted !', 'Close', { duration: 3000 });
+        this.router.navigate(['sessions']);
+      }
       );
   }
 
@@ -69,7 +69,7 @@ export class DetailComponent implements OnInit {
       .detail(this.sessionId)
       .subscribe((session: Session) => {
         this.session = session;
-        this.isParticipate = session.users.some(u => u === this.sessionService.sessionInformation!.id);
+        this.isParticipate = session.users.some(user => user === this.sessionService.sessionInformation!.id);
         this.teacherService
           .detail(session.teacher_id.toString())
           .subscribe((teacher: Teacher) => this.teacher = teacher);
