299cb82b27f02f2a478fbf73a8d68a6e47f95d7c
Kevin Renault <kevin.renault.92@gmail.com>
2026-01-02 10:38:46 +0100

    refactor(error): remove custom error types and simplify handler

--- back/src/main/java/com/openclassrooms/starterjwt/exception/GlobalExceptionHandler.java
+++ back/src/main/java/com/openclassrooms/starterjwt/exception/GlobalExceptionHandler.java
@@ -5,10 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import com.openclassrooms.starterjwt.dto.ErrorResponse;
-
-import java.time.LocalDateTime;
-
 /**
  * Gestionnaire global des exceptions de l'application.
  * Intercepte et formate les exceptions pour retourner des réponses d'erreur
@@ -20,34 +16,15 @@ import java.time.LocalDateTime;
 public class GlobalExceptionHandler {
 
     /**
-     * Gère toutes les autres exceptions non interceptées spécifiquement.
-     * Détermine le code HTTP approprié selon le type d'exception.
+     * Gère toutes les autres exceptions NumberFormatException spécifiquement.
+     * Détermine le code HTTP approprié.
      * 
      * @param ex L'exception levée
      * @return Une réponse HTTP avec le code de statut approprié
      */
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex) {
-        HttpStatus status;
-        if (ex instanceof ApiException exception) {
-            status = exception.getHttpStatus();
-        } else if (ex instanceof NumberFormatException) {
-            status = HttpStatus.BAD_REQUEST;
-        } else {
-            status = HttpStatus.INTERNAL_SERVER_ERROR;
-        }
-        return buildErrorResponse(status, ex.getMessage());
-    }
-
-    private ResponseEntity<ErrorResponse> buildErrorResponse(HttpStatus statusCode,
-            String errorMessage) {
-        ErrorResponse errorResponse = new ErrorResponse(
-                LocalDateTime.now(),
-                statusCode.value(),
-                errorMessage);
-
-        return ResponseEntity
-                .status(statusCode)
-                .body(errorResponse);
+    @ExceptionHandler(NumberFormatException.class)
+    public ResponseEntity<?> handleGlobalException(NumberFormatException ex) {
+        // return buildErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 }
