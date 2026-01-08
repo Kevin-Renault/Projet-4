18075a912396d515888f70a107fdae361460b6be
Kevin-Renault <kevin.renault.92@gmail.com>
2026-01-08 14:42:35 +0100

    Config: Setup Cypress e2e with coverage instrumentation

--- front/cypress.config.ts
+++ front/cypress.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'cypress'
+import setupNodeEvents from './cypress/plugins/index.ts'
 
 export default defineConfig({
   videosFolder: 'cypress/videos',
@@ -6,11 +7,7 @@ export default defineConfig({
   fixturesFolder: 'cypress/fixtures',
   video: false,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.ts').default(on, config)
-    },
+    setupNodeEvents,
     baseUrl: 'http://localhost:4200',
   },
 })
