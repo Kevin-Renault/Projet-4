1a6e947c22a72ab8f4592f62d44a5b6e9c0959aa
Kevin-Renault <kevin.renault.92@gmail.com>
2026-01-21 16:01:10 +0100

    test: add unit tests for UserDetailsImpl equality and account status methods

--- back/src/test/java/com/openclassrooms/starterjwt/security/services/UserDetailsImplTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/security/services/UserDetailsImplTest.java
@@ -5,87 +5,108 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class UserDetailsImplTest {
 
-    @Test
-    public void testEquals_SameObject() {
-        // Arrange
-        UserDetailsImpl user = UserDetailsImpl.builder()
-                .id(1L)
-                .username("test")
-                .build();
+        @Test
+        public void testEquals_SameObject() {
+                UserDetailsImpl user = UserDetailsImpl.builder()
+                                .id(1L)
+                                .username("test")
+                                .build();
+                assertTrue(user.equals(user));
+        }
 
-        // Act & Assert
-        assertTrue(user.equals(user));
-    }
+        @Test
+        public void testIsAccountNonExpired() {
+                UserDetailsImpl user = UserDetailsImpl.builder().build();
+                assertTrue(user.isAccountNonExpired());
+        }
 
-    @Test
-    public void testEquals_NullObject() {
-        // Arrange
-        UserDetailsImpl user = UserDetailsImpl.builder()
-                .id(1L)
-                .username("test")
-                .build();
+        @Test
+        public void testIsAccountNonLocked() {
+                UserDetailsImpl user = UserDetailsImpl.builder().build();
+                assertTrue(user.isAccountNonLocked());
+        }
 
-        // Act & Assert
-        assertFalse(user.equals(null));
-    }
+        @Test
+        public void testIsCredentialsNonExpired() {
+                UserDetailsImpl user = UserDetailsImpl.builder().build();
+                assertTrue(user.isCredentialsNonExpired());
+        }
 
-    @Test
-    public void testEquals_DifferentClass() {
-        // Arrange
-        UserDetailsImpl user = UserDetailsImpl.builder()
-                .id(1L)
-                .username("test")
-                .build();
+        @Test
+        public void testIsEnabled() {
+                UserDetailsImpl user = UserDetailsImpl.builder().build();
+                assertTrue(user.isEnabled());
+        }
 
-        // Act & Assert
-        assertFalse(user.equals("string"));
-    }
+        @Test
+        public void testEquals_NullObject() {
+                // Arrange
+                UserDetailsImpl user = UserDetailsImpl.builder()
+                                .id(1L)
+                                .username("test")
+                                .build();
 
-    @Test
-    public void testEquals_SameId() {
-        // Arrange
-        UserDetailsImpl user1 = UserDetailsImpl.builder()
-                .id(1L)
-                .username("test1")
-                .build();
-        UserDetailsImpl user2 = UserDetailsImpl.builder()
-                .id(1L)
-                .username("test2")
-                .build();
+                // Act & Assert
+                assertFalse(user.equals(null));
+        }
 
-        // Act & Assert
-        assertTrue(user1.equals(user2));
-    }
+        @Test
+        public void testEquals_DifferentClass() {
+                // Arrange
+                UserDetailsImpl user = UserDetailsImpl.builder()
+                                .id(1L)
+                                .username("test")
+                                .build();
 
-    @Test
-    public void testEquals_DifferentId() {
-        // Arrange
-        UserDetailsImpl user1 = UserDetailsImpl.builder()
-                .id(1L)
-                .username("test")
-                .build();
-        UserDetailsImpl user2 = UserDetailsImpl.builder()
-                .id(2L)
-                .username("test")
-                .build();
+                // Act & Assert
+                assertFalse(user.equals("string"));
+        }
 
-        // Act & Assert
-        assertFalse(user1.equals(user2));
-    }
+        @Test
+        public void testEquals_SameId() {
+                // Arrange
+                UserDetailsImpl user1 = UserDetailsImpl.builder()
+                                .id(1L)
+                                .username("test1")
+                                .build();
+                UserDetailsImpl user2 = UserDetailsImpl.builder()
+                                .id(1L)
+                                .username("test2")
+                                .build();
 
-    @Test
-    public void testEquals_NullId() {
-        // Arrange
-        UserDetailsImpl user1 = UserDetailsImpl.builder()
-                .id(null)
-                .username("test")
-                .build();
-        UserDetailsImpl user2 = UserDetailsImpl.builder()
-                .id(1L)
-                .username("test")
-                .build();
+                // Act & Assert
+                assertTrue(user1.equals(user2));
+        }
 
-        // Act & Assert
-        assertFalse(user1.equals(user2));
-    }
+        @Test
+        public void testEquals_DifferentId() {
+                // Arrange
+                UserDetailsImpl user1 = UserDetailsImpl.builder()
+                                .id(1L)
+                                .username("test")
+                                .build();
+                UserDetailsImpl user2 = UserDetailsImpl.builder()
+                                .id(2L)
+                                .username("test")
+                                .build();
+
+                // Act & Assert
+                assertFalse(user1.equals(user2));
+        }
+
+        @Test
+        public void testEquals_NullId() {
+                // Arrange
+                UserDetailsImpl user1 = UserDetailsImpl.builder()
+                                .id(null)
+                                .username("test")
+                                .build();
+                UserDetailsImpl user2 = UserDetailsImpl.builder()
+                                .id(1L)
+                                .username("test")
+                                .build();
+
+                // Act & Assert
+                assertFalse(user1.equals(user2));
+        }
 }
