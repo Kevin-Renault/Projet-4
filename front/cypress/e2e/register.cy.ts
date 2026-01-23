b7cd8aa996e4804b3b8a5123f4d773d889e3536c
Kevin-Renault <kevin.renault.92@gmail.com>
2026-01-23 15:44:25 +0100

    test(e2e): replace hardcoded parameters with shared constants in registrer.cy.ts

--- front/cypress/e2e/register.cy.ts
+++ front/cypress/e2e/register.cy.ts
@@ -1,7 +1,8 @@
 
 
 
-import { EMAIL_FIELD, PASSWORD_FIELD, FIRST_NAME_FIELD, LAST_NAME_FIELD, SUBMIT_BUTTON } from './selectors';
+import { EMAIL_FIELD, PASSWORD_FIELD, FIRST_NAME_FIELD, LAST_NAME_FIELD, SUBMIT_BUTTON, DISABLED, NOT_DISABLED } from './selectors';
+import { NEW_USER_FIRST_NAME, NEW_USER_LAST_NAME, NEW_USER_EMAIL, NEW_USER_PASSWORD, YOGA_USER_EMAIL, YOGA_USER_PASSWORD, INVALID_EMAIL, VALID_EMAIL, INVALID_PASSWORD } from './test-data';
 
 
 describe('Register spec', () => {
@@ -15,10 +16,10 @@ describe('Register spec', () => {
       },
     })
 
-    cy.get(FIRST_NAME_FIELD).type("John")
-    cy.get(LAST_NAME_FIELD).type("Doe")
-    cy.get(EMAIL_FIELD).type("user@studio.com")
-    cy.get(PASSWORD_FIELD).type(`${"test!1234"}{enter}`)
+    cy.get(FIRST_NAME_FIELD).type(NEW_USER_FIRST_NAME)
+    cy.get(LAST_NAME_FIELD).type(NEW_USER_LAST_NAME)
+    cy.get(EMAIL_FIELD).type(NEW_USER_EMAIL)
+    cy.get(PASSWORD_FIELD).type(`${NEW_USER_PASSWORD}{enter}`)
     cy.url().should('include', '/login')
   })
 
@@ -27,10 +28,10 @@ describe('Register spec', () => {
     cy.visit('/register')
     cy.intercept('POST', '/api/auth/register').as('registerRequest')
 
-    cy.get(FIRST_NAME_FIELD).type("John")
-    cy.get(LAST_NAME_FIELD).type("Doe")
-    cy.get(EMAIL_FIELD).type("yoga@studio.com")
-    cy.get(PASSWORD_FIELD).type(`${"test!1234"}{enter}{enter}`)
+    cy.get(FIRST_NAME_FIELD).type(NEW_USER_FIRST_NAME)
+    cy.get(LAST_NAME_FIELD).type(NEW_USER_LAST_NAME)
+    cy.get(EMAIL_FIELD).type(YOGA_USER_EMAIL)
+    cy.get(PASSWORD_FIELD).type(`${YOGA_USER_PASSWORD}{enter}{enter}`)
 
     cy.wait('@registerRequest').its('response.statusCode').should('eq', 400)
     cy.url().should('include', '/register')
@@ -41,35 +42,35 @@ describe('Register spec', () => {
     cy.visit('/register')
 
     // Test paramètre non renseigné
-    cy.get(FIRST_NAME_FIELD).type("John")
-    cy.get(EMAIL_FIELD).type("valid@email.com")
-    cy.get(PASSWORD_FIELD).type("test!1234")
-    cy.get(SUBMIT_BUTTON).should('be.disabled')
+    cy.get(FIRST_NAME_FIELD).type(NEW_USER_FIRST_NAME)
+    cy.get(EMAIL_FIELD).type(VALID_EMAIL)
+    cy.get(PASSWORD_FIELD).type(YOGA_USER_PASSWORD)
+    cy.get(SUBMIT_BUTTON).should(DISABLED)
 
     // Test mot de passe trop court
-    cy.get(FIRST_NAME_FIELD).type("John")
-    cy.get(LAST_NAME_FIELD).type("Doe")
-    cy.get(EMAIL_FIELD).clear().type("valid@email.com")
-    cy.get(PASSWORD_FIELD).clear().type("12")  // < 3 caractères
-    cy.get(SUBMIT_BUTTON).should('be.disabled')
+    cy.get(FIRST_NAME_FIELD).type(NEW_USER_FIRST_NAME)
+    cy.get(LAST_NAME_FIELD).type(NEW_USER_LAST_NAME)
+    cy.get(EMAIL_FIELD).clear().type(VALID_EMAIL)
+    cy.get(PASSWORD_FIELD).clear().type(INVALID_PASSWORD)  // < 3 caractères
+    cy.get(SUBMIT_BUTTON).should(DISABLED)
 
 
     // Test email invalide
-    cy.get(EMAIL_FIELD).clear().type("invalid-email.com")  // Email sans @
-    cy.get(PASSWORD_FIELD).clear().type("test!1234")
-    cy.get(SUBMIT_BUTTON).should('be.disabled')
+    cy.get(EMAIL_FIELD).clear().type(INVALID_EMAIL)  // Email sans @
+    cy.get(PASSWORD_FIELD).clear().type(YOGA_USER_PASSWORD)
+    cy.get(SUBMIT_BUTTON).should(DISABLED)
 
 
     // Test prénom trop court
     cy.get(FIRST_NAME_FIELD).clear().type("A")  // < 3 caractères
-    cy.get(SUBMIT_BUTTON).should('be.disabled')
+    cy.get(SUBMIT_BUTTON).should(DISABLED)
 
     // Avec données valides, bouton activé
-    cy.get(FIRST_NAME_FIELD).clear().type("John")
-    cy.get(LAST_NAME_FIELD).clear().type("Doe")
-    cy.get(EMAIL_FIELD).clear().type("valid@email.com")
-    cy.get(PASSWORD_FIELD).clear().type("test!1234")
-    cy.get(SUBMIT_BUTTON).should('not.be.disabled')
+    cy.get(FIRST_NAME_FIELD).clear().type(NEW_USER_FIRST_NAME)
+    cy.get(LAST_NAME_FIELD).clear().type(NEW_USER_LAST_NAME)
+    cy.get(EMAIL_FIELD).clear().type(VALID_EMAIL)
+    cy.get(PASSWORD_FIELD).clear().type(YOGA_USER_PASSWORD)
+    cy.get(SUBMIT_BUTTON).should(NOT_DISABLED)
   })
 
 });
