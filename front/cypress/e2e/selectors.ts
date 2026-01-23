0aeb9c40d03587cc72959c22c5f491b5e6dffdec
Kevin-Renault <kevin.renault.92@gmail.com>
2026-01-23 15:34:40 +0100

    test(e2e): add and centralize shared constants for tests

--- front/cypress/e2e/selectors.ts
+++ front/cypress/e2e/selectors.ts
@@ -4,3 +4,13 @@ export const PASSWORD_FIELD = 'input[formControlName=password]';
 export const FIRST_NAME_FIELD = 'input[formControlName=firstName]';
 export const LAST_NAME_FIELD = 'input[formControlName=lastName]';
 export const SUBMIT_BUTTON = 'button[type=submit]';
+export const DISABLED = 'be.disabled';
+export const NOT_DISABLED = 'not.be.disabled';
+
+// Sélecteurs pour le formulaire de session
+export const SESSION_NAME_FIELD = 'input[formControlName=name]';
+export const SESSION_DATE_FIELD = 'input[formControlName=date]';
+export const SESSION_TEACHER_FIELD = 'mat-select[formControlName=teacher_id]';
+export const SESSION_DESCRIPTION_FIELD = 'textarea[formControlName=description]';
+export const SESSION_SUBMIT_BUTTON = 'button[type=submit]';
+
