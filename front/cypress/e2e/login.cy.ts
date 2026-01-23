55d7eb7d00abd9edccbf8ea78d29528ec76541a0
Kevin-Renault <kevin.renault.92@gmail.com>
2026-01-23 13:37:26 +0100

    Tests: Add comprehensive e2e tests for login

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -1,8 +1,9 @@
 describe('Login spec', () => {
-  it('Login successfull', () => {
+  it('Login successfull (Mock Api)', () => {
     cy.visit('/login')
 
     cy.intercept('POST', '/api/auth/login', {
+      statusCode: 200,
       body: {
         id: 1,
         username: 'userName',
@@ -23,5 +24,52 @@ describe('Login spec', () => {
     cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
 
     cy.url().should('include', '/sessions')
+  })
+
+  it('Login successfull (real API)', () => {
+    cy.visit('/login')
+
+    cy.intercept('POST', '/api/auth/login').as('loginRequest')
+    cy.get('input[formControlName=email]').type("yoga@studio.com")
+    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+    cy.wait('@loginRequest').its('response.statusCode').should('eq', 200)
+    cy.url().should('include', '/sessions')
+  })
+
+  it('Login fail (Mock Api)', () => {
+    cy.visit('/login')
+
+    cy.intercept('POST', '/api/auth/login', {
+      statusCode: 401,
+      body: {
+        message: 'Invalid credentials'
+      },
+    }).as('loginRequest')
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/session',
+      },
+      []).as('session')
+
+    cy.get('input[formControlName=email]').type("yoga@studio.net")
+    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+
+    cy.wait('@loginRequest')
+    cy.url().should('include', '/login')
+  })
+
+  it('Login fail (real API)', () => {
+    cy.visit('/login')
+
+    cy.intercept('POST', '/api/auth/login').as('loginRequest')
+
+    cy.get('input[formControlName=email]').type("wrong@email.com")
+    cy.get('input[formControlName=password]').type(`${"wrongpassword"}{enter}{enter}`)
+
+    cy.wait('@loginRequest').its('response.statusCode').should('eq', 401)
+    cy.url().should('include', '/login')
+    cy.contains('An error occurred').should('be.visible')
   })
 });
